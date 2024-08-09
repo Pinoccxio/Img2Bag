0ba30962556ccad9ac19e9deeb194c67a997297a
陈昕 <chenxin_2001@163.com>
2024-08-09 13:45:18 +0800

    Delete build folders

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cx/IRMV_ws/bevfusion_ws/src/Img2Bag
-# Build directory: /home/cx/IRMV_ws/bevfusion_ws/src/Img2Bag/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
