6fa340fe341eff5c47eaf77e3dfa8215376f355f
陈昕 <chenxin_2001@163.com>
2024-10-09 12:50:34 +0800

    only save source file

--- build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/cx/IRMV_ws/bevfusion_ws/src/Img2Bag/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
